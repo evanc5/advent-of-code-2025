190d190f657a5a95dff92c8cb5599cf340c005d1
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-02 21:31:03 -0500

    Day 1 - Part 2 initial naive solution (still not quite right)

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -13,8 +13,6 @@ static void Part1()
 
     foreach (var line in input)
     {
-        Console.WriteLine($"Start {pos} Turn {line}");
-
         var dir = line[0];
         var distance = int.Parse(line.Substring(1));
 
@@ -40,13 +38,57 @@ static void Part1()
         {
             count++;
         }
-        Console.WriteLine($"End {pos} Count {count}");
     }
 
-    Console.WriteLine($"Final {count}");
+    Console.WriteLine($"Part 1 {count}");
 }
 
 static void Part2()
 {
+    var input = File.ReadAllLines("input.txt");
 
+    int pos = 50;
+    int count = 0;
+
+    const int minPos = 0;
+    const int maxPos = 99;
+
+    foreach (var line in input)
+    {
+        var dir = line[0];
+        var distance = int.Parse(line.Substring(1));
+
+        if (dir == 'L')
+        {
+            pos -= distance;
+        }
+        else if (dir == 'R')
+        {
+            pos += distance;
+        }
+
+        while (pos < minPos)
+        {
+            pos += maxPos + 1;
+            if (pos != 0)
+            {
+                count++;
+            }
+        }
+        while (pos > maxPos)
+        {
+            pos -= maxPos + 1;
+            if (pos != 0)
+            {
+                count++;
+            }
+        }
+
+        if (pos == 0)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"Part 2 {count}");
 }
