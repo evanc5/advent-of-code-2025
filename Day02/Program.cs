e45189e56342cb48f35d06617cbc2384ed435551
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 11:14:48 -0500

    start part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -30,7 +30,22 @@ static void Part1()
 
 static void Part2()
 {
-    var input = File.ReadAllText("input.txt");
+    var input = File.ReadAllText("input.txt").Split(',');
+
+    long total = 0;
+
+    foreach (var item in input)
+    {
+        var split = item.Split('-');
+        var start = long.Parse(split[0]);
+        var end = long.Parse(split[1]);
+
+        for (long i = start; i <= end; i++)
+        {
+            var number = i.ToString();
+            // add to total if the number contains a sequence of digits that appear at least twice, e.g. 123123123, 55555, etc
+        }
+    }
 
     Console.WriteLine($"Part 2 ");
 }
