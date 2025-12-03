f06fd5a4b988e24f5c2d6888f8a89b17b97c2515
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 11:51:23 -0500

    Day 3 Part 1 (#2)

---
+++ Day03/Program.cs
@@ -0,0 +1,29 @@
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllLines("input.txt");
+
+    var total = 0;
+
+    foreach (var line in input)
+    {
+        var digits = line.Select(c => int.Parse(c.ToString())).ToArray();
+
+        var firstMax = digits[..^1].Max();
+        var firstMaxIndex = digits.IndexOf(firstMax);
+        var secondMax = digits[(firstMaxIndex + 1)..].Max();
+
+        total += int.Parse($"{firstMax}{secondMax}");
+    }
+
+    Console.WriteLine($"Part 1 {total}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines("input.txt");
+
+    Console.WriteLine($"Part 2 ");
+}
