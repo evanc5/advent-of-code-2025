3d3866c5e751d3f67d85af578a1bae1e11dda3b0
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 10:52:08 -0500

    start day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,16 @@
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllText("input.txt");
+
+    Console.WriteLine($"Part 1 ");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllText("input.txt");
+
+    Console.WriteLine($"Part 2 ");
+}
