ca18bc5ada4f8c6e72d6f790fd7b1103ff043c03
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 11:10:22 -0500

    part 1 solution

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -3,9 +3,29 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllText("input.txt");
+    var input = File.ReadAllText("input.txt").Split(',');
 
-    Console.WriteLine($"Part 1 ");
+    long total = 0;
+
+    foreach (var item in input)
+    {
+        var split = item.Split('-');
+        var start = long.Parse(split[0]);
+        var end = long.Parse(split[1]);
+
+        for (long i = start; i <= end; i++)
+        {
+            var number = i.ToString();
+            var firstHalf = number[..(number.Length / 2)];
+            var secondHalf = number[(number.Length / 2)..];
+            if (firstHalf == secondHalf)
+            {
+                total += i;
+            }
+        }
+    }
+
+    Console.WriteLine($"Part 1 {total}");
 }
 
 static void Part2()
