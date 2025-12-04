a03812e706835fb6d5db88698858e4c146aacf17
Evan C <evanc5@users.noreply.github.com>
2025-12-04 00:31:12 -0500

    interpret input as a 2D char array

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -3,7 +3,7 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.ToArray()).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     int total = 0;
@@ -15,7 +15,7 @@ static void Part1()
 
 static void Part2()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.ToArray()).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     int total = 0;
