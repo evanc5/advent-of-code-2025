f46e37a038d027a20e73a5b11f30ace89e1ef9b2
Evan C <evanc5@users.noreply.github.com>
2025-12-04 00:45:23 -0500

    implement day 4 part 1

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -8,6 +8,41 @@ static void Part1()
 
     int total = 0;
 
+    for (int row = 0; row < input.Length; row++)
+    {
+        for (int col = 0; col < input[row].Length; col++)
+        {
+            char current = input[row][col];
+            if (current != '@')
+            {
+                continue;
+            }
+
+            int currentCount = 0;
+
+            for (int r = row - 1; r <= row + 1; r++)
+            {
+                for (int c = col - 1; c <= col + 1; c++)
+                {
+                    if (r < 0 || r >= input.Length || c < 0 || c >= input[row].Length)
+                        continue;
+                    if (r == row && c == col)
+                        continue;
+
+                    char neighbor = input[r][c];
+                    if (neighbor == '@')
+                    {
+                        currentCount++;
+                    }
+                }
+            }
+            if (currentCount < 4)
+            {
+                total++;
+            }
+        }
+    }
+
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 1: {total}");
     System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
