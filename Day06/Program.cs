504427682e6cec72379cffd2c67a6ec5bc4ef2a6
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 22:35:02 -0500

    add template file to days 4-12

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllLines("input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    int total = 0;
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 1: {total}");
+    System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines("input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    int total = 0;
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 2: {total}");
+    System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
