d677bb407f40c5c8ac73bf45c3755b5524c20b90
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-03 22:30:13 -0500

    complete part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -27,6 +27,30 @@ static void Part1()
 static void Part2()
 {
     var input = File.ReadAllLines("input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    Console.WriteLine($"Part 2 ");
+    long total = 0;
+    const int batteryCount = 12;
+
+    foreach (var line in input)
+    {
+        var digits = line.Select(c => int.Parse(c.ToString())).ToArray();
+
+        int digitsFound = 0;
+        int nextIndex = 0;
+        int[] selectedDigits = new int[batteryCount];
+        while (digitsFound < batteryCount)
+        {
+            var searchSpace = digits[nextIndex..^(batteryCount - digitsFound - 1)];
+            var max = searchSpace.Max();
+            nextIndex = searchSpace.IndexOf(max) + nextIndex + 1;
+            selectedDigits[digitsFound] = max;
+            digitsFound++;
+        }
+        total += long.Parse(string.Concat(selectedDigits));
+    }
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 2: {total}");
+    System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
 }
