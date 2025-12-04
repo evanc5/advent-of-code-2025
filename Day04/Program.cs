bca529d0e29c8097b89b7c9ddb62a3565ac34afa
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-04 00:45:59 -0500

    Implement Day04 Part 1 (#6)
    * interpret input as a 2D char array
    * implement day 4 part 1

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -3,10 +3,45 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.ToArray()).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     int total = 0;
+
+    for (int row = 0; row < input.Length; row++)
+    {
+        for (int col = 0; col < input[row].Length; col++)
+        {
+            char current = input[row][col];
+            if (current != '@')
+            {
+                continue;
+            }
+
+            int currentCount = 0;
+
+            for (int r = row - 1; r <= row + 1; r++)
+            {
+                for (int c = col - 1; c <= col + 1; c++)
+                {
+                    if (r < 0 || r >= input.Length || c < 0 || c >= input[row].Length)
+                        continue;
+                    if (r == row && c == col)
+                        continue;
+
+                    char neighbor = input[r][c];
+                    if (neighbor == '@')
+                    {
+                        currentCount++;
+                    }
+                }
+            }
+            if (currentCount < 4)
+            {
+                total++;
+            }
+        }
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 1: {total}");
@@ -15,7 +50,7 @@ static void Part1()
 
 static void Part2()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.ToArray()).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     int total = 0;
