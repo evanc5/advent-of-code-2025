14d4bc3f74850e4d4364499f584d052c03cfcc6f
Evan C <evanc5@users.noreply.github.com>
2025-12-04 00:37:45 -0500

    refactor day 3

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -6,17 +6,12 @@ static void Part1()
     var input = File.ReadAllLines("input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    var total = 0;
+    long total = 0;
+    const int batteryCount = 2;
 
     foreach (var line in input)
     {
-        var digits = line.Select(c => int.Parse(c.ToString())).ToArray();
-
-        var firstMax = digits[..^1].Max();
-        var firstMaxIndex = digits.IndexOf(firstMax);
-        var secondMax = digits[(firstMaxIndex + 1)..].Max();
-
-        total += int.Parse($"{firstMax}{secondMax}");
+        total += ProcessLine(line, batteryCount);
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
@@ -34,23 +29,30 @@ static void Part2()
 
     foreach (var line in input)
     {
-        var digits = line.Select(c => int.Parse(c.ToString())).ToArray();
-
-        int digitsFound = 0;
-        int nextIndex = 0;
-        int[] selectedDigits = new int[batteryCount];
-        while (digitsFound < batteryCount)
-        {
-            var searchSpace = digits[nextIndex..^(batteryCount - digitsFound - 1)];
-            var max = searchSpace.Max();
-            nextIndex = searchSpace.IndexOf(max) + nextIndex + 1;
-            selectedDigits[digitsFound] = max;
-            digitsFound++;
-        }
-        total += long.Parse(string.Concat(selectedDigits));
+        total += ProcessLine(line, batteryCount);
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {total}");
     System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
+
+static long ProcessLine(string line, int batteryCount)
+{
+    var digits = line.Select(c => int.Parse(c.ToString())).ToArray();
+    int[] selectedDigits = new int[batteryCount];
+
+    int digitsFound = 0;
+    int nextIndex = 0;
+
+    while (digitsFound < batteryCount)
+    {
+        var searchSpace = digits[nextIndex..^(batteryCount - digitsFound - 1)];
+        var max = searchSpace.Max();
+        nextIndex = searchSpace.IndexOf(max) + nextIndex + 1;
+        selectedDigits[digitsFound] = max;
+        digitsFound++;
+    }
+
+    return long.Parse(string.Concat(selectedDigits));
 }
