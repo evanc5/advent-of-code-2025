39de2d859f83ca1decae593c6b544629052aee2d
Evan C <evanc5@users.noreply.github.com>
2025-12-05 17:22:25 -0500

    start part 2 (works for example but not actual)

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -37,15 +37,52 @@ static void Part2()
     var input = File.ReadAllLines("input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    long total = 0;
 
     var freshLines = input.Where(line => line.Contains('-'));
+
+    var ranges = new List<(long Start, long End)>();
 
     foreach (var freshLine in freshLines)
     {
         var split = freshLine.Split('-');
         var start = long.Parse(split[0]);
         var end = long.Parse(split[1]);
+
+        ranges.Add((start, end));
+    }
+
+    var matchFound = true;
+    while (matchFound)
+    {
+        matchFound = false;
+        for (int i = 0; i < ranges.Count; i++)
+        {
+            for (int j = i + 1; j < ranges.Count; j++)
+            {
+                var rangeA = ranges[i];
+                var rangeB = ranges[j];
+
+                if (rangeA.Start <= rangeB.End && rangeB.Start <= rangeA.End)
+                {
+                    var newStart = Math.Min(rangeA.Start, rangeB.Start);
+                    var newEnd = Math.Max(rangeA.End, rangeB.End);
+                    ranges[i] = (newStart, newEnd);
+                    ranges.RemoveAt(j);
+                    matchFound = true;
+                    break;
+                }
+            }
+            if (matchFound)
+            {
+                break;
+            }
+        }
+    }
+
+    foreach (var range in ranges)
+    {
+        total += (int)(range.End - range.Start + 1);
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
