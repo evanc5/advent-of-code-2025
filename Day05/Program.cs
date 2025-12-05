4f714768cbd9645b738fb8d3868c53002e2afc9e
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-05 00:15:00 -0500

    complete part 1 (#8)

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -6,10 +6,29 @@ static void Part1()
     var input = File.ReadAllLines("input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    int count = 0;
+
+    var freshLines = input.Where(line => line.Contains('-'));
+    var availableIngredients = input.Where(line => !string.IsNullOrWhiteSpace(line) && !line.Contains('-')).Select(long.Parse);
+
+    foreach (var availableIngredient in availableIngredients)
+    {
+        foreach (var freshLine in freshLines)
+        {
+            var split = freshLine.Split('-');
+            var start = long.Parse(split[0]);
+            var end = long.Parse(split[1]);
+
+            if (availableIngredient >= start && availableIngredient <= end)
+            {
+                count++;
+                break;
+            }
+        }
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
-    Console.WriteLine($"Part 1: {total}");
+    Console.WriteLine($"Part 1: {count}");
     System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
 }
 
