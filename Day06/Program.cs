0071c14c6b4f45a2a015b45154ef5a406d217f4d
Evan C <evanc5@users.noreply.github.com>
2025-12-06 00:12:16 -0500

    finish day 6 part 1

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -3,10 +3,23 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    long total = 0;
+    for (int i = 0; i < input[0].Length; i++)
+    {
+        var op = input[4][i];
+        switch (op)
+        {
+            case "+":
+                total += long.Parse(input[0][i]) + long.Parse(input[1][i]) + long.Parse(input[2][i]) + long.Parse(input[3][i]);
+                break;
+            case "*":
+                total += long.Parse(input[0][i]) * long.Parse(input[1][i]) * long.Parse(input[2][i]) * long.Parse(input[3][i]);
+                break;
+        }
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 1: {total}");
