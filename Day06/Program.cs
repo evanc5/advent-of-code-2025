528d873938a93ee8808d1b2d072f1688899d79c0
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-07 10:59:15 -0500

    implement day 6 part 2 (#10)

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -31,7 +31,42 @@ static void Part2()
     var input = File.ReadAllLines("input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    long total = 0;
+
+    var numberLines = input.Where(l => l.Any(char.IsDigit)).ToArray();
+    var ops = input.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    var currentOp = 0;
+    var lineTotal = ops[currentOp] == "+" ? 0L : 1L;
+
+    for (int i = 0; i < numberLines[0].Length; i++)
+    {
+        var digits = "";
+        foreach (var line in numberLines)
+        {
+            digits += line[i];
+        }
+
+        digits = digits.Replace(" ", "");
+
+        if (digits.Length == 0)
+        {
+            total += lineTotal;
+            currentOp++;
+            lineTotal = ops[currentOp] == "+" ? 0L : 1L;
+            continue;
+        }
+
+        switch (ops[currentOp])
+        {
+            case "+":
+                lineTotal += long.Parse(digits);
+                break;
+            case "*":
+                lineTotal *= long.Parse(digits);
+                break;
+        }
+    }
+    total += lineTotal;
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {total}");
