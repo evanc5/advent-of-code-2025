e2c59e55233922083a48031ccf9337b6bb419d44
Evan C <evanc5@users.noreply.github.com>
2025-12-09 00:17:28 -0500

    implement part 1

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -3,13 +3,24 @@ Part2();
 
 static void Part1()
 {
-    var input = File.ReadAllLines("input.txt");
+    var input = File.ReadAllLines("input.txt").Select(line => line.Split(',').Select(long.Parse).ToArray()).ToArray();
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    var maxArea = long.MinValue;
+    for (int i = 0; i < input.Length; i++)
+    {
+        for (int j = i + 1; j < input.Length; j++)
+        {
+            var area = Area(input[i], input[j]);
+            if (area > maxArea)
+            {
+                maxArea = area;
+            }
+        }
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
-    Console.WriteLine($"Part 1: {total}");
+    Console.WriteLine($"Part 1: {maxArea}");
     System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
 }
 
@@ -23,4 +34,9 @@ static void Part2()
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {total}");
     System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
+
+static long Area(long[] p1, long[] p2)
+{
+    return (Math.Abs(p1[0] - p2[0]) + 1) * (Math.Abs(p1[1] - p2[1]) + 1);
 }
