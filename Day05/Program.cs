9822185d6aaa59787007d4d02bfd3cdccc2622d1
Evan C <11150077+evanc5@users.noreply.github.com>
2025-12-09 10:24:49 -0500

    Complete Day 5 Part 2 (#13)
    * initial setup for part 2
    * set up initial loop for part 2
    * start part 2 (works for example but not actual)
    * combine ranges just outside a neighbor
    * fix cast to int (should be long)

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -37,7 +37,54 @@ static void Part2()
     var input = File.ReadAllLines("input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
-    int total = 0;
+    long total = 0;
+
+    var freshLines = input.Where(line => line.Contains('-'));
+
+    var ranges = new List<(long Start, long End)>();
+
+    foreach (var freshLine in freshLines)
+    {
+        var split = freshLine.Split('-');
+        var start = long.Parse(split[0]);
+        var end = long.Parse(split[1]);
+
+        ranges.Add((start, end));
+    }
+
+    var matchFound = true;
+    while (matchFound)
+    {
+        matchFound = false;
+        for (int i = 0; i < ranges.Count; i++)
+        {
+            for (int j = i + 1; j < ranges.Count; j++)
+            {
+                var (start1, end1) = ranges[i];
+                var (start2, end2) = ranges[j];
+
+                if (start1 <= end2 && start2 <= end1 ||
+                    start1 == end2 + 1 || start2 == end1 + 1)
+                {
+                    var newStart = Math.Min(start1, start2);
+                    var newEnd = Math.Max(end1, end2);
+                    ranges[i] = (newStart, newEnd);
+                    ranges.RemoveAt(j);
+                    matchFound = true;
+                    break;
+                }
+            }
+            if (matchFound)
+            {
+                break;
+            }
+        }
+    }
+
+    foreach (var (start, end) in ranges)
+    {
+        total += end - start + 1;
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {total}");
